c584254ad5b70960687736667f9bf3458fc18fba
Alexis Carlier <alexiscarlier@Alexiss-MacBook.local>
2017-12-20 13:37:10 +0000

    Add test for _rollScore, change total function

--- spec/frameSpec.js
+++ spec/frameSpec.js
@@ -1,6 +1,17 @@
 describe("A frame of bowling", function(){
-  it("calculates a total for two roles", function(){
-     var frame = new Frame([2, 4]);
-     expect(frame.total()).toEqual(6)
+
+  describe('#total', function() {
+    it("calculates a total for a normal role", function(){
+       var frame = new Frame([2, 4]);
+       expect(frame.total()).toEqual(6)
+    });
   });
+
+  describe('#_rollScore', function() {
+    it("returns the sum of the rolls", function(){
+      var frame = new Frame([2, 4]);
+      expect(frame._rollScore()).toEqual(6)
+    });
+  });
+
 });
